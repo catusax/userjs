206e43adb5426c9b48df51588276469bbf949e4e
coolrc136 <coolrc136@users.noreply.github.com>
2021-08-16 22:57:23 +0800

    Update bilibili_fullscreen.js

--- js/bilibili_fullscreen.js
+++ js/bilibili_fullscreen.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bilibili_fullscreen
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @include	     *bilibili.com*
